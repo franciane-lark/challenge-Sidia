e1810940cc1c1fd9fb5ae3bfcd3a6ba964a0c8a7
Franciane Lark <franciane.lark019@gmail.com>
2021-08-30 16:49:01 -0400

    updates-pasties

--- project-lark/challenge/javascript/challenge-1.js
+++ project-lark/challenge/javascript/challenge-1.js
@@ -1,13 +1,29 @@
+<!-- CRUD Node Js -->
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CRUD</title>
+    <title>Consume API (omdbapi)</title>
   </head>
   <body>
-    <!-- CRUD  -->
-    <script></script>
+    <!-- http://www.omdbapi.com/?i=tt3896198&apikey=4ab221b7 -->
+    <script>
+      var crudApp = new (function () {
+      // An array of JSON objects with values.
+      this.GuardiansOfGalaxy = [
+          "Metascore": "67",
+          "imdbRating": "7.6",
+          "imdbVotes": "600,709",
+          "imdbID": "tt3896198",
+          "Type": "movie",
+          "DVD": "10 Jul 2017",
+          "BoxOffice": "$389,813,101",
+          "Production": "Marvel Studios, Walt Disney Pictures",
+          "Website": "N/A",
+          "Response": "True"
+      ]
+    </script>
   </body>
 </html>
